3504603a5450ca95fc8c0ae8fd28fc8b2a1cdcd6
Marius Preikschat <m.preikschat@googlemail.com>
2021-06-12 14:01:26 +0200

    show hex value of color

--- RandomColors/ColorsDetailVC.swift
+++ RandomColors/ColorsDetailVC.swift
@@ -14,6 +14,7 @@ class ColorsDetailVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = color ?? .white
+    navigationItem.title = view.backgroundColor?.toHex()
   }
 
 }
